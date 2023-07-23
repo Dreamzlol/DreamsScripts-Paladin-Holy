ea4018f0b3af3b5f8b72af7ddcaa2a427fd604a0
Dreamzlol <denis.dasilvarocha@icloud.com>
2023-07-23 20:47:34 +0200

    ✅ Added Engineering Gloves and Trinkets

--- pve/apl.lua
+++ pve/apl.lua
@@ -1,6 +1,38 @@
 local Unlocker, awful, rotation = ...
 local holy = rotation.paladin.holy
-local player = awful.player
+local player, target = awful.player, awful.target
+
+local function engineering_gloves()
+    local UseInventoryItem = awful.unlock("UseInventoryItem")
+    local getItemCooldown = GetInventoryItemCooldown("player", 10)
+
+    if target and target.exists then
+        if target.level == -1 and getItemCooldown == 0 then
+            if UseInventoryItem(10) then
+                awful.alert("Hyperspeed Accelerators", 54758)
+                return
+            end
+        end
+    end
+end
+
+local function trinket()
+    local UseInventoryItem = awful.unlock("UseInventoryItem")
+    local getItemCooldown13 = GetInventoryItemCooldown("player", 13)
+    local getItemCooldown14 = GetInventoryItemCooldown("player", 14)
+
+    if target and target.exists then
+        if target.level == -1 then
+            if getItemCooldown13 == 0 then
+                UseInventoryItem(13)
+                return
+            elseif getItemCooldown14 == 0 then
+                UseInventoryItem(14)
+                return
+            end
+        end
+    end
+end
 
 function rotation.APL_PvE()
     if player.mounted or
@@ -29,6 +61,8 @@ function rotation.APL_PvE()
     holy.DivinePlea()
     holy.BeaconOfLight()
     holy.SacredShield()
+    engineering_gloves()
+    trinket()
     holy.HolyLight("incinerate_flesh")
     holy.HolyLight("friend")
     holy.HolyLight("tank")
